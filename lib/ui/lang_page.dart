86ae1fe36b50664f3bff8febc54472bd7eb34966
Mks30d <manish30d@gmail.com>
2025-08-06 12:38:00 +0530

    first commit

---
+++ lib/ui/lang_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LangPage extends StatelessWidget {
+  const LangPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
